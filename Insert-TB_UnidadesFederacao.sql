78621ffb16b769a1c33f23d8a88a1a5b3f0d8185
wpferreira01 <wpferreira01@gmail.com>
2018-12-03 04:06:26 +0000

    Alterando arquivos

---
+++ Insert-TB_UnidadesFederacao.sql
@@ -0,0 +1,33 @@
+SET IDENTITY_INSERT [dbo].[TB_UnidadesFederacao] ON
+
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (1, 'Rondonia', 'RO', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (2, 'Acre', 'AC', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (3, 'Amazonas', 'AM', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (4, 'Roraima', 'RR', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (5, 'Para', 'PA', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (6, 'Amapa', 'AP', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (7, 'Tocantins', 'TO', 'Norte')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (8, 'Maranhao', 'MA', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (9, 'Piaui', 'PI', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (10, 'Ceara', 'CE', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (11, 'Rio Grande do Norte', 'RN', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (12, 'Paraiba', 'PB', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (13, 'Pernambuco', 'PE', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (14, 'Alagoas', 'AL', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (15, 'Sergipe', 'SE', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (16, 'Bahia', 'BA', 'Nordeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (17, 'Minas Gerais', 'MG', 'Sudeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (18, 'Espirito Santo', 'ES', 'Sudeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (19, 'Rio de Janeiro', 'RJ', 'Sudeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (20, 'Sao Paulo', 'SP', 'Sudeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (21, 'Parana', 'PR', 'Sul')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (22, 'Santa Catarina', 'SC', 'Sul')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (23, 'Rio Grande do Sul', 'RS', 'Sul')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (24, 'Mato Grosso do Sul', 'MS', 'Centro-Oeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (25, 'Mato Grosso', 'MT', 'Centro-Oeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (26, 'Goias', 'GO', 'Centro-Oeste')
+INSERT INTO [dbo].[TB_UnidadesFederacao] ([ID], [DESCR], [SiglaUF], [RegiaoBrasil]) VALUES (27, 'Distrito Federal', 'DF', 'Centro-Oeste')
+
+SET IDENTITY_INSERT [dbo].[TB_UnidadesFederacao] OFF
+
+
